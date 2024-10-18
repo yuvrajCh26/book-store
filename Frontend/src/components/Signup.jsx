fb5fd00f5eddcaf2fe021d0f18fcb8814a84fe2b
yuvrajCh26 <yuvraj.2101168ec@iiitbh.ac.in>
2024-10-18 23:31:06 +0530

    updated backend url

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/signup", userInfo)
+      .post("https://book-store-d2nk.onrender.com/user/signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
